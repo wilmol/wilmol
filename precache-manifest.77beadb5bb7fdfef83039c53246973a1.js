9f6a1130834520b76e0fea7ee9238bbaa738e6d8
wilmol <wilmol@users.noreply.github.com>
2020-06-19 06:06:15 +0000

    deploy: 65e2ce1366adbb460103f2c79b1aab072d28fdbe

---
+++ precache-manifest.77beadb5bb7fdfef83039c53246973a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a169b7aad4a6405d619c61d66e65b80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da2cf771546ade604227",
+    "url": "/static/css/main.ae7b0720.chunk.css"
+  },
+  {
+    "revision": "2a06f3dd4d812133b826",
+    "url": "/static/js/2.6ce0637b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6ce0637b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2cf771546ade604227",
+    "url": "/static/js/main.7e4c8ce9.chunk.js"
+  },
+  {
+    "revision": "98a4f9fcc3cb2e3938eb",
+    "url": "/static/js/runtime-main.f8d2c74f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
