87fba15896e612d15cd8ccaeccc56a8983dd4511
Will Molloy <willjoemolloy@gmail.com>
2021-03-06 07:48:03 +1300

    Import new font
    Lato, same as one used in latex cv.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import './App.scss';
 const App: React.FC = () => {
   return (
     <div className="App">
+      <div className="Title">
+        <h1>Will Molloy</h1>
+        <p>Software Engineer</p>
+      </div>
       <div className="Socials">
         <a href="https://github.com/wilmol" target="_blank" rel="noopener noreferrer">
           <img src={githubLogo} className="Social-logo" alt="github logo and link" />
